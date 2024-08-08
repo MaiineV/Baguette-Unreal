14b5cedd8adb4e2361b0088ba1a6ef65637b7b00
Ignacio Martinez <agustinmartinezbarrios2@hotmail.com>
2024-08-08 20:20:00 -0300

    Project created on 5.4 C++

---
+++ Source/Baguette/BaguetteGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BaguetteGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABaguetteGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABaguetteGameMode();
+};
+
+
+
